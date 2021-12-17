da578b47e098f976c79ce0ca3f7584b94cabdcf0
bwassim <bwassim23@gmail.com>
2021-12-17 21:45:45 +0100

    test again

--- starter/main.py
+++ starter/main.py
@@ -12,7 +12,6 @@ from starter.starter.ml.model import inference
 if "DYNO" in os.environ and os.path.isdir(".dvc"):
     os.system("dvc config core.no_scm true")
     os.system("dvc config core.hardlink_lock true")
-    os.system("dvc config cache.type copy")
     if os.system("dvc pull -r s3remote") != 0:
         exit("dvc pull failed")
     os.system("rm -r .dvc .apt/usr/lib/dvc")
